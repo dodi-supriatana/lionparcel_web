83bb50b5d2585fb3607caf9fe09a897a45f49b06
dodi to <dodisupriatna8@gmail.com>
2019-06-24 19:10:51 +0700

    update maps

--- application/views/layout/leanding_header.php
+++ application/views/layout/leanding_header.php
@@ -7,8 +7,8 @@
 ================================================== -->
 <link rel="stylesheet" href="<?php echo base_url('assets/leandingpage/css/style.css') ?>">
 <link rel="stylesheet" href="<?php echo base_url('assets/leandingpage/css/main-color.css') ?>" id="colors">
-<link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.7/css/select2.min.css" rel="stylesheet" />
-<script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.7/js/select2.min.js"></script>
+<!-- <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.7/css/select2.min.css" rel="stylesheet" />
+<script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.7/js/select2.min.js"></script> -->
 <link rel="shortcut icon" href="<?php echo base_url('assets/leandingpage/images/logonoupper.png') ?>" type="image/x-icon">
 </head>
 
