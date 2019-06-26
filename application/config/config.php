5492c4dc2797c905959558b673175591c1e7d03e
dodi to <dodisupriatna8@gmail.com>
2019-06-26 14:04:06 +0700

    config

--- application/config/config.php
+++ application/config/config.php
@@ -26,7 +26,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 // $config['base_url'] = 'http://lionparcel.cudo.co.id/';
 // $config['base_url'] = 'http://localhost/lionparcel_web/';
 
-$config['base_url'] = 'http://localhost/lionparcel_jolly/';
+$config['base_url'] = 'http://lionparcel.cudo.co.id/';
+// $config['base_url'] = 'http://localhost/lionparcel_jolly/';
 
 
 /*
