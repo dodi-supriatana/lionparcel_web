471a8f6587c17f789d689ee32425c7646b4c0213
adithia alam <adhityalam@gmail.com>
2019-08-02 16:33:11 +0700

    UPDATE, Login POP-UP di menu AGEN dan merubah tampilan LOGIN Menu

--- application/views/layout/leanding_header.php
+++ application/views/layout/leanding_header.php
@@ -65,7 +65,7 @@
 					<div id="sign-in-dialog" class="zoom-anim-dialog mfp-hide">
 
 						<div class="small-dialog-header">
-							<h3>Sign In</h3>
+							<img src="<?php echo base_url('assets/icon/logonew.png')?>" width="299px">
 						</div>
 
 						<!--Tabs -->
@@ -80,30 +80,14 @@
 
 								<!-- Login -->
 								<div class="tab-content" id="tab1" style="display: none;">
-									<form method="post" class="login">
-
-										<p class="form-row form-row-wide">
-											<label for="username">Username:
-												<i class="im im-icon-Male"></i>
-												<input type="text" class="input-text" name="username" id="username" value="" />
-											</label>
-										</p>
-
-										<p class="form-row form-row-wide">
-											<label for="password">Password:
-												<i class="im im-icon-Lock-2"></i>
-												<input class="input-text" type="password" name="password" id="password" />
-											</label>
-											<span class="lost_password">
-												<a href="#">Lost Your Password?</a>
-											</span>
-										</p>
-
-										<div class="form-row">
-											<input type="submit" class="button border margin-top-5" name="login" value="Login" />
-											<div class="checkboxes margin-top-10">
-												<input id="remember-me" type="checkbox" name="check">
-												<label for="remember-me">Remember Me</label>
+									<form>
+										<div>
+											<img src="<?php echo base_url('assets/leandingpage/images/loginagen.png')?>" >
+										</div>
+										<br>
+										<div>
+											<div class="col-md-12">
+												<button type="submit" class="btn btn-warning btn-block btn-flat" href="<?php echo base_url('leanding/tracking') ?>" style=" background:#d7282d; border-color:#d7282d; border-width: 2px; color: #fff; font-size:18px; border-radius: 40px;">Masuk sebagai agen Lionparcel</button>
 											</div>
 										</div>
 
